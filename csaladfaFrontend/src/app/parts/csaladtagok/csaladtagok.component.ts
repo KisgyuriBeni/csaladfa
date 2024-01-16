1bc00f41e95914c2a3be454069bce4c48f4378a9
KisgyorgyBendeguz <kisgyorgybeni@gmail.com>
2024-01-16 22:48:47 +0100

    adatok kiiratasa a csaladtagok komponensen sikeres

--- csaladfaFrontend/src/app/parts/csaladtagok/csaladtagok.component.ts
+++ csaladfaFrontend/src/app/parts/csaladtagok/csaladtagok.component.ts
@@ -10,6 +10,15 @@ import { BaseService } from 'src/app/services/base.service';
 export class CsaladtagokComponent {
   
   csaladtagok:any
+  oszlopok=[
+    {key:"nev", text:"Név"},
+    {key:"szul_datum", text:"Születési dátum"},
+    {key:"szul_hely", text:"Születési helye"},
+    {key:"anyja_neve", text:"Anyja neve"},
+    {key:"apja_neve", text:"Apja neve"},
+    {key:"halal_hely", text:"Halál helye"},
+    {key:"halal_datum", text:"Halál Dátuma"}
+  ]
 
   constructor(public base:BaseService){
 
